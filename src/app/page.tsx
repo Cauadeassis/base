70866374a0f18fd3cbdf49b824a64082a9093dc0
Cauã <cauadeassis2009@gmail.com>
2026-03-07 17:53:38 +0000

    Creates table `users`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,117 @@
-import Image from 'next/image';
-import styles from './styles.module.css';
-export default function Home() {
+"use client";
+
+import { useState } from "react";
+import styles from "./styles.module.css";
+import EmailField from "../components/emailField";
+import PasswordField from "../components/passwordField";
+
+type Step =
+  | "login"
+  | "forgotPassword"
+  | "verifyingCode"
+  | "newPassword"
+  | "success";
+
+export default function Login() {
+  const [step, setStep] = useState<Step>("login");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className={styles.body}>
-      <header>
-        <h1></h1>
-        <nav></nav>
-      </header>
-      <main>
-        <section></section>
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <section></section>
-      </main>
-      <aside></aside>
-      <footer></footer>
+      <div className={styles.siteContent}>
+        <header>
+        </header>
+
+        <main>
+          <form>
+            {step === "login" && (
+              <>
+                <EmailField value={email} onChange={setEmail} />
+
+                <PasswordField value={password} onChange={setPassword} />
+
+                <button
+                  type="button"
+                  className={styles.forgotPassword}
+                  onClick={() => setStep("forgotPassword")}
+                >
+                  Esqueceu a senha?
+                </button>
+
+                <button type="submit" className={styles.mainButton}>
+                  Entrar
+                </button>
+              </>
+            )}
+
+            {step === "forgotPassword" && (
+              <>
+                <EmailField value={email} onChange={setEmail} />
+
+                <button
+                  type="button"
+                  onClick={() => setStep("verifyingCode")}
+                  className={styles.mainButton}
+                >
+                  Enviar código
+                </button>
+              </>
+            )}
+
+            {step === "verifyingCode" && (
+              <>
+                <label>
+                  Código de verificação
+                  <input
+                    type="text"
+                    inputMode="numeric"
+                    maxLength={5}
+                    placeholder="12345"
+                    required
+                  />
+                </label>
+
+                <button
+                  type="button"
+                  onClick={() => setStep("newPassword")}
+                  className={styles.mainButton}
+                >
+                  Verificar código
+                </button>
+              </>
+            )}
+
+            {step === "newPassword" && (
+              <>
+                <PasswordField value={password} onChange={setPassword} />
+
+                <button
+                  type="button"
+                  onClick={() => setStep("success")}
+                  className={styles.mainButton}
+                >
+                  Criar nova senha
+                </button>
+              </>
+            )}
+
+            {step === "success" && (
+              <>
+                <p>Senha redefinida com sucesso 🎉</p>
+
+                <button
+                  type="button"
+                  onClick={() => setStep("login")}
+                  className={styles.mainButton}
+                >
+                  Voltar para login
+                </button>
+              </>
+            )}
+          </form>
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/infra/migrations/1772905366051_create-users-table.js
@@ -0,0 +1,27 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.sql(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      email TEXT UNIQUE NOT NULL,
+      password TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW()
+    );
+  `);
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => { };
